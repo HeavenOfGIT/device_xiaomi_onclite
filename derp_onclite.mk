b8e01ea67fd84159939526a5c42f277d1be06cde
NurKeinNeid <mralexman3000@gmail.com>
2021-04-07 21:40:16 +0200

    onclite: Enable build type Official for DerpFest
    Change-Id: I4dc8751199363afed0c613a8330d965760da21f0
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- derp_onclite.mk
+++ derp_onclite.mk
@@ -16,6 +16,8 @@ $(call inherit-product, device/xiaomi/onclite/device.mk)
 # Inherit some common DerpFest stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
+DERP_BUILDTYPE := Official
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_onclite
 PRODUCT_DEVICE := onclite
